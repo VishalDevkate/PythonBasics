d80df765f814c49ff219b1965960e3d5ecdc3723
VishalDevkate <v.devkate@gmail.com>
2026-04-06 19:39:16 +0530

    conditions and loops

--- Demo2.py
+++ Demo2.py
@@ -1,3 +1,4 @@
+#List
 values = [2, "hi", 3.14, True]
 print(values)
 
@@ -12,3 +13,29 @@ print(values[1:3])
 
 values.insert(1,"Rahul")
 print(values)
+
+#Tuple
+val = (1, 2, 3)
+
+print(val[0])
+print(val)
+
+#val[1] = 7  #error
+
+#dictionary
+dict = {
+    "a": 1,
+    2: 3.14,
+    3: "Rahul"
+}
+
+print(dict)
+
+#print(dict[0]) #keyerror
+print(dict['a'])
+print(dict[2])
+
+dict["d"] = 'Hello'
+
+print(dict.keys())
+print(dict.values())
