e146834c2407f96772bc249c4593aff54400f0c1
VishalDevkate <v.devkate@gmail.com>
2026-04-07 00:34:53 +0530

    Oops

---
+++ OOPsDemo.py
@@ -0,0 +1,16 @@
+class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        print("constructor executed")
+
+    def add(self, num1, num2):
+        print("adding two numbers")
+        print("addition of passed numbers: ", num1 + num2)
+        return self.num1 + self.num2
+
+    def subtract(self, num1, num2):
+        return self.num1 - self.num2
+
+c1 = Calculator(1, 2)
+print(c1.add(3,4))
