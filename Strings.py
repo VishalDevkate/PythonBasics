6c59831bedf51645e6030d4d434a85c12ec5bae7
VishalDevkate <v.devkate@gmail.com>
2026-04-08 10:19:30 +0530

    Strings

---
+++ Strings.py
@@ -0,0 +1,11 @@
+str1 = " This is Pune city"
+str2 = "Pune"
+str3 = "punecity"
+
+print("str2 in str1: ", str2 in str1)
+print("str3 in str1: ", str3 in str1)
+
+str4 = " Stripping "
+print("ltrim: ", str4.lstrip())
+print("rtrim: ", str4.rstrip())
+print("strip: ", str4.strip())
